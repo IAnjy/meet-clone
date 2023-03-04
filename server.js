f51a26d060b3f174bfeba2fa8c286c94fa6b8958
IAnjy <ianjymartial@gmail.com>
2023-03-04 21:59:07 +0300

    create audio and video streaming and fixing some issues

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const path = require("path");
 var app = express();
-var server = app.listen(3000, function(){
-    console.log("listening on port 3000");
+var server = app.listen(3000, function () {
+  console.log("listening on port 3000");
 });
 
-const io = require("socket.io")(server); 
-app.use(express.static(path.join(__dirname,"")));
+const io = require("socket.io")(server);
+app.use(express.static(path.join(__dirname, "")));
 
+var userConnections = [];
 io.on("connection", (socket) => {
-    console.log("socket id is ", socket.id);
-})
+  console.log("socket id is ", socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnent", data.displayName, data.meetingid);
+
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+    socket.emit("inform_me_about_other_user", other_users);
+  });
+
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
+});
